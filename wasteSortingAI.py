7c2f1b6db56290fa59b037e87d5cc70c52f107c6
WilliamB-EPS <wbowers@eastsideprep.org>
2022-12-27 13:25:39 -0800

    NEW DATASET

--- wasteSortingAI.py
+++ wasteSortingAI.py
@@ -7,10 +7,14 @@ import seaborn as sns
 import torch
 
 # importing datasets
+import keras as k
 from keras.models import Sequential
 from keras.utils import to_categorical, plot_model               #     
 from keras.layers import Conv2D, Dense, Flatten, MaxPooling2D    # the actual layers that will go into the cnn
 from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.utils import class_weight
+
 
 
 
@@ -18,15 +22,16 @@ from sklearn.model_selection import train_test_split
 
 images = []
 labels = []
-img_res = [200, 200]
-categories = ['Compost', 'Recycle', 'Trash']
+img_res = [100, 100]
+categories = ['battery','biological','brown-glass', "cardboard", 'clothes', 'green-glass', 'metal', 'paper', 'plastic', 'shoes', 
+'trash', 'white-glass']
 weird_count = 0
 count = 0
 for i in range(len(categories)):
     for filename in os.listdir(os.getcwd() + '/ml-data/' + categories[i] + '/'):
         image = im.open(os.getcwd() + '/ml-data/' + categories[i] + '/' + filename)
         np_img = np.array(image.resize((img_res[1], img_res[0])))
-        if np_img.shape == (200, 200, 3):
+        if np_img.shape == (img_res[0], img_res[1], 3):
             images.append(np_img)
             labels.append(i)
             count += 1
@@ -38,10 +43,6 @@ X_train = np.array(X_train)
 X_test = np.array(X_test)
 y_train = np.array(y_train)
 y_test = np.array(y_test)
-print(X_train.shape)
-print(y_train.shape)
-print(X_test.shape)
-print(y_test.shape)
 
 # some preprocessing on the data
 
@@ -53,6 +54,28 @@ X_test = X_test.astype('float32') / 255
 # so you can compare the two
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
+
+"""
+
+Random oversampling
+
+
+# reshape data into 2d arrays so it can be processed
+nsamples, nx, ny, d3 = X_train.shape
+X_train = X_train.reshape((nsamples,d3*nx*ny))
+
+# Randomly oversample the minority class (garbage)
+ros = RandomOverSampler(random_state=42)
+X_train_ros, y_train_ros= ros.fit_resample(X_train, y_train)
+
+# resize data back into original shape
+nsamples = X_train_ros.shape[0]
+X_train_ros = X_train_ros.reshape((nsamples, nx, ny, d3))
+
+X_train = X_train_ros
+y_train = y_train_ros
+
+"""
 
 # creates the framework for the neural network
 cnn = Sequential()
@@ -70,7 +93,8 @@ cnn.add(MaxPooling2D(pool_size=(2,2)))
 # flattens the layering output into a 1D array
 cnn.add(Flatten())
 cnn.add(Dense(units=128, activation='relu'))            # units = how many outputs
-cnn.add(Dense(units=3, activation='softmax'))          # units is 3 because one for each number from 1-3
+cnn.add(Dense(units=len(categories), activation='softmax'))          # units is 3 because one for each number from 1-3
+
 
 # print out a summary of the neural network 
 print(cnn.summary())
@@ -80,44 +104,39 @@ print("\n")
 # sort of puts everything else in place
 cnn.compile(optimizer='adam',                   
             loss ='categorical_crossentropy',   
-            metrics=['accuracy'])               
+            metrics=['accuracy', k.metrics.AUC()])               
 
 # actually running the cnn and fitting/training neural network on the data
 # batch size = after 64 samples, make a small adjustment
 # epoch = every time all the data is run through, make a big adjustment
 
-cnn.fit(X_train, y_train, epochs=10, batch_size=32, validation_split=0.1)
+print('labels info')
+print(y_train[0])
+
+
+history = cnn.fit(X_train, y_train, epochs=15, batch_size=8, validation_split=0.1)
 
 # evalutating the loss/accuracy of the model on the test set
-loss, accuracy = cnn.evaluate(X_test, y_test)
+loss, accuracy, auc = cnn.evaluate(X_test, y_test)
 print("loss / accuracy:")
 print(loss)
 print(accuracy)
 
-# print out the prediction for the first sample in the test set
-predictions = cnn.predict(X_test)
-print(y_test[0]) 
+acc = history.history['accuracy'] # get history report of the model
 
-print("testing some samples:")
+val_acc = history.history['val_accuracy'] # get history of the validation set
 
-for index, probability in enumerate(predictions[0]):
-    print(f'{index}: {probability:.10%}')
+loss = history.history['loss'] #get the history of the lossses recorded on the train set
+val_loss = history.history['val_loss'] #get the history of the lossses recorded on the validation set
 
-print("actual value:")
+plt.figure(figsize=(8, 8)) # set figure size for the plot generated
 
-print(y_test[0])
 
-# visualize 24 random results
-sns.set(font_scale=1)
-index = np.random.choice(np.arange(len(X_test)), 24, replace=False)    # pick 24 random smamples
-figure, axes = plt.subplots(nrows=4, ncols=6, figsize=(16,9))           # set dimensions
-for item in zip(axes.ravel(), X_test[index], y_test[index], predictions[index]):          # put each sample into a "slot" in the table
-    axes, image, target, predict = item
-    axes.imshow(image, cmap=plt.cm.gray_r)
-    axes.set_xticks([])
-    axes.set_yticks([])
-    print(target)
-    axes.set_title("label: " + categories[np.argmax(target)] + '''
-    predicted: ''' + categories[np.argmax(predict)])
-plt.tight_layout()
-plt.show()                      # prints it out
+plt.plot(acc, label='Training Accuracy') #plot accuracy curve for each train run
+plt.plot(val_acc, label='Validation Accuracy') #plot accuracy curve for each validation run
+
+plt.legend(loc='lower right')
+plt.ylabel('Accuracy') #label name for y axis
+plt.ylim([min(plt.ylim()), 1]) #set limit for y axis
+plt.title('Training and Validation Accuracy') #set title for the plot
+plt.show()
