c59ab829a3a1819d9a4c7852f7ba5cd75a52e235
WilliamB-EPS <wbowers@eastsideprep.org>
2023-01-19 15:34:03 -0800

    new data and code - 72% accuracy

--- wasteSortingAI.py
+++ wasteSortingAI.py
@@ -1,28 +1,23 @@
-import sys
+from collections import Counter
 from PIL import Image as im
 import numpy as np
 from matplotlib import pyplot as plt
 import os
-import seaborn as sns
 import sklearn
-import torch
-import random
-
-# importing datasets
 import keras as k
 import tensorflow as tf
-from keras import regularizers
 from keras.models import Sequential
-from keras.utils import to_categorical, plot_model                    
-from keras.layers import Conv2D, Dense, Flatten, MaxPooling2D, RandomFlip, RandomRotation, Dropout
+from keras.utils import to_categorical                 
+from keras.layers import Conv2D, Dense, Flatten, MaxPooling2D, RandomRotation, RandomFlip, RandomZoom, RandomContrast
 from sklearn.model_selection import train_test_split
-from imblearn.over_sampling import RandomOverSampler
-tf.config.set_soft_device_placement(True) 
-
+from imblearn.over_sampling import SMOTE
 from PIL import ImageFile
+from sklearn.metrics import classification_report
+
+tf.config.set_soft_device_placement(True) 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-# dataset 1
+# load dataset
 
 images = []
 labels = []
@@ -48,6 +43,24 @@ X_test = np.array(X_test)
 y_train = np.array(y_train)
 y_test = np.array(y_test)
 
+
+# reshape data into 2d arrays so it can be processed
+nsamples, nx, ny, d3 = X_train.shape
+X_train = X_train.reshape((nsamples,d3*nx*ny))
+
+# Randomly oversample minority classes
+X_train_ros, y_train_ros= SMOTE().fit_resample(X_train, y_train)
+
+# Check distribution of data across classes
+print(sorted(Counter(y_train_ros).items()))
+
+# resize data back into original shape
+nsamples = X_train_ros.shape[0]
+X_train_ros = X_train_ros.reshape((nsamples, nx, ny, d3))
+
+X_train = X_train_ros
+y_train = y_train_ros
+
 # some preprocessing on the data
 
 # normalizing the data to be from 0-1 instead 1-255
@@ -60,26 +73,14 @@ y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 
 
-# reshape data into 2d arrays so it can be processed
-nsamples, nx, ny, d3 = X_train.shape
-X_train = X_train.reshape((nsamples,d3*nx*ny))
-
-# Randomly oversample minority classes to even out data 
-ros = RandomOverSampler()
-X_train_ros, y_train_ros= ros.fit_resample(X_train, y_train)
-
-# resize data back into original shape
-nsamples = X_train_ros.shape[0]
-X_train_ros = X_train_ros.reshape((nsamples, nx, ny, d3))
-
-X_train = X_train_ros
-y_train = y_train_ros
-
 # data augmentation
 with tf.device('/cpu:0'):
     data_augmentation = k.Sequential([
-    RandomFlip("horizontal_and_vertical", input_shape=(img_res[0], img_res[1], 3)),
-    RandomRotation(0.4),
+    # RandomFlip("horizontal_and_vertical", input_shape=(img_res[0], img_res[1], 3)),
+    RandomRotation(0.2, input_shape=(img_res[0], img_res[1], 3)),
+    RandomFlip("horizontal_and_vertical"),
+    RandomZoom(0.2, 0.2),
+    RandomContrast(factor=0.2)
     ])
 
 # creates the framework for the neural network
@@ -95,7 +96,6 @@ cnn = Sequential(
     MaxPooling2D(pool_size=(2,2)),
     Flatten(),
     Dense(units=128, activation='relu'),
-    Dropout(0.5),
     Dense(units=len(categories), activation='softmax')
     ]
 )
@@ -110,15 +110,16 @@ X_train, y_train = sklearn.utils.shuffle(X_train, y_train)
 # batch size = after 64 samples, make a small adjustment
 # epoch = every time all the data is run through, make a big adjustment
 with tf.device("/gpu:0"):
-    cnn.compile(optimizer='adam',                   
-            loss ='categorical_crossentropy',   
-            metrics=['accuracy'])               
-    history = cnn.fit(X_train, y_train, epochs=6, batch_size=16, validation_split=0.1)
+    cnn.compile(optimizer='adam', 
+                loss=k.losses.CategoricalCrossentropy(),
+                metrics=[k.metrics.CategoricalCrossentropy(name='categorical_crossentropy'),'accuracy'])           
+    history = cnn.fit(X_train, y_train, epochs=8, batch_size=16, validation_split=0.1)
 
 # evalutating the loss/accuracy of the model on the test set
-loss, accuracy = cnn.evaluate(X_test, y_test)
-print("loss / accuracy:")
+loss, crossent, accuracy = cnn.evaluate(X_test, y_test)
+print("loss / crossentropy / accuracy:")
 print(loss)
+print(crossent)
 print(accuracy)
 
 acc = history.history['accuracy'] # get history report of the model
@@ -128,14 +129,21 @@ val_acc = history.history['val_accuracy'] # get history of the validation set
 loss = history.history['loss'] #get the history of the lossses recorded on the train set
 val_loss = history.history['val_loss'] #get the history of the lossses recorded on the validation set
 
-plt.figure(figsize=(8, 8)) # set figure size for the plot generated
+y_pred = cnn.predict(X_test)
+
+y_pred = (y_pred > 0.5) 
+
+print(classification_report(y_test, y_pred, target_names=categories, digits=4))
 
 
-plt.plot(acc, label='Training Accuracy') #plot accuracy curve for each train run
-plt.plot(val_acc, label='Validation Accuracy') #plot accuracy curve for each validation run
+# plt.figure(figsize=(8, 8)) # set figure size for the plot generated
 
-plt.legend(loc='lower right')
-plt.ylabel('Accuracy') #label name for y axis
-plt.ylim([min(plt.ylim()), 1]) #set limit for y axis
-plt.title('Training and Validation Accuracy') #set title for the plot
-plt.show()
+
+# plt.plot(acc, label='Training Accuracy') #plot accuracy curve for each train run
+# plt.plot(val_acc, label='Validation Accuracy') #plot accuracy curve for each validation run
+
+# plt.legend(loc='lower right')
+# plt.ylabel('Accuracy') #label name for y axis
+# plt.ylim([min(plt.ylim()), 1]) #set limit for y axis
+# plt.title('Training and Validation Accuracy') #set title for the plot
+# plt.show()
